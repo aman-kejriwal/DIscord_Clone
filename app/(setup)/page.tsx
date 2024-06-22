24deb752211bd04a9bafd3d4f5b78b60b41d742d
aman-kejriwal <84925133+aman-kejriwal@users.noreply.github.com>
2024-06-22 16:38:20 +0530

    Creating initial Profile

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,41 @@
+// import { Button } from "@/components/ui/button";
+// import { UserButton } from "@clerk/nextjs";
+// import { ModeToggle } from "@/components/mode-toggle";
+// export default function Home() {
+//   return ( 
+//     <div className="flex flex-col">
+//       <UserButton
+//       afterSignOutUrl="/sign-in"
+//       />
+//       <ModeToggle/>
+//     </div>
+//   );
+// }
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
+
+const SetUpPage = async () => {
+  const profile=await initialProfile();
+  console.log(profile);
+  // we are checking if the user is a part of any server, if he is then redirect to them o/w ask them to create a ner server
+  const server=await db.server.findFirst({
+    where:{
+      members:{
+        some:{
+          profileId:profile?.id
+        }
+      }
+    }
+  });
+  if(server){
+     return redirect(`/server/${server.id}`);
+  }
+
+  return <div>
+    <h1>Create a server</h1>
+  </div>
+  return <div>{initialProfile.name}</div>
+}
+ 
+export default SetUpPage;
